42527e2b7062c5b607e1c87f0ed54af7389b1154
홍의 <honge7694@naver.com>
2026-03-17 22:10:09 +0900

    refactor: [LOG] 로그 포맷팅 변경

--- src/custom-logger/core/winston.config.ts
+++ src/custom-logger/core/winston.config.ts
@@ -1,9 +1,9 @@
-import * as winston from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file';
 import * as crypto from 'crypto';
-import { inspect } from 'util';
 import { ClsServiceManager } from 'nestjs-cls';
 import { getTodayString } from 'src/common/utils/date.util';
+import { inspect } from 'util';
+import * as winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 // 개발/운영 환경 로그 레벨 분기
 const isLocal = process.env.NODE_ENV === 'local' || !process.env.NODE_ENV;
@@ -25,6 +25,7 @@ const colors = {
 	white: (text: string): string => `\x1b[37m${text}\x1b[0m`, // 기본 텍스트
 };
 
+// 콘솔 포맷
 const consoleFormat = winston.format.printf((info) => {
 	const { timestamp, level, message, context, ...meta } = info;
 
@@ -59,27 +60,77 @@ const consoleFormat = winston.format.printf((info) => {
 	const formattedContext = context ? `[${context}]` : '[App]';
 	const coloredMessage = colors.cyan(String(message));
 
-	return `${coloredUuid} ${coloredTimestamp} ${emoji} ${level} ${formattedContext} - ${coloredMessage} ${metaString}`;
+	return `${coloredTimestamp} ${emoji} ${level.toUpperCase()} ${coloredUuid} ${formattedContext} - ${coloredMessage} ${metaString}`;
+});
+
+// 로그 파일 포맷 (파일에서는 색상 제거)
+const uncolorizedFileFormat = winston.format.printf((info) => {
+	const { timestamp, level, message, context, ...meta } = info;
+
+	// 심볼 제거 및 순수 메타데이터 추출
+	const cleanMeta = Object.fromEntries(Object.entries(meta));
+	let metaString = '';
+	if (Object.keys(cleanMeta).length > 0) {
+		metaString =
+			'\n' +
+			inspect(cleanMeta, {
+				colors: false,
+				depth: 4,
+				breakLength: 80,
+			});
+	}
+
+	const cls = ClsServiceManager.getClsService();
+	const traceId: string = cls.isActive() ? cls.get('traceId') : 'N/A';
+	const rawUuid = traceId === 'N/A' ? crypto.randomUUID() : traceId;
+	const formattedContext = context ? `[${context}]` : '[App]';
+	const shortUuid = rawUuid.replace(/-/g, '').substring(0, 10);
+
+	return `[${shortUuid}] ${timestamp} ${level.toUpperCase()} ${formattedContext} - ${message} ${metaString}`;
+});
+
+// 평문
+const fileFormat = winston.format.combine(winston.format.timestamp(), uncolorizedFileFormat);
+
+// UUID 주입
+const injectTraceId = winston.format((info) => {
+	const cls = ClsServiceManager.getClsService();
+	info.traceId = cls.isActive() ? cls.get('traceId') : 'N/A';
+
+	return info;
+});
+
+// 파일 저장을 하면 줄바꿈이 생기는 현상 방지
+const stripNewlineForFile = winston.format((info) => {
+	// TypeORM 컨텍스트에서 발생한 메시지일 경우에만 작동
+	if (info.context === 'TypeORM' && typeof info.message === 'string') {
+		info.message = info.message.replace(/\s+/g, ' ').trim();
+	}
+
+	return info;
 });
 
 // 공통 파일 포맷 (JSON)
-const fileFormat = winston.format.combine(
+/* const fileFormat = winston.format.combine(
 	winston.format.timestamp(),
+	injectTraceId(), // UUID 주입
+	stripNewlineForFile(), // 쿼리 줄바꿈 제거
 	winston.format.json(), // JSON 형태로 저장
-);
+); */
 
 // Console 출력 포맷(local을 포함한 전체)
 const transports: winston.transport[] = [
 	new winston.transports.Console({
 		level: level,
-		format: winston.format.combine(winston.format.colorize(), consoleFormat),
+		format: consoleFormat,
 	}),
 ];
 
 const exceptionHandlers: winston.transport[] = [];
 const rejectionHandlers: winston.transport[] = [];
 
-if (!isLocal) {
+// if (!isLocal) {
+if (true) {
 	transports.push(
 		// info 이상 로그 메시지 일자별 파일 생성
 		new DailyRotateFile({
@@ -131,7 +182,7 @@ if (!isLocal) {
 
 export const coreLogger = winston.createLogger({
 	level,
-	format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston.format.json()),
+	format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' })),
 	// 콘솔 출력 설정
 	transports,
 	exceptionHandlers,
